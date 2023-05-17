17fb0aace651892724ba919ba1d00166ea919f60
Küster <fw4-bbc-u1@fh-h.de>
2023-05-17 18:59:57 +0200

    Helloworld

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Klasse Helloworld.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+
+public class Helloworld
+{
+    public static void main (String args[]){
+    System.out.println("Helloworld");
+    }
+}
